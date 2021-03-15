f97f8581b35fd14b1b133803e87059da7b90e6db
Zubareva-Anna <a84.zubareva@gmail.com>
2021-03-15 23:07:49 +0300

    13

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         double price = 1590.99;
-        int miles = service.calculate (price);
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
